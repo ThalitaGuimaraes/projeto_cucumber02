c101578a726c234e6bbe68d32dc438e7ac7c9dc7
ThalitaGuimaraes <thalita_guimaraesrj@hotmail.com>
2023-05-26 08:46:18 -0300

    Desenvolvimento dos testes com selenium webdriver

--- src/test/java/cotiinformatica/CadastrarCategoriasStepDefinitions.java
+++ src/test/java/cotiinformatica/CadastrarCategoriasStepDefinitions.java
@@ -1,30 +1,42 @@
 package cotiinformatica;
 
-	import io.cucumber.java.pt.Dado;
-	import io.cucumber.java.pt.E;
-	import io.cucumber.java.pt.Então;
-	import io.cucumber.java.pt.Quando;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-	public class CadastrarCategoriasStepDefinitions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-		@Dado("Acessar a página de cadastro de categoria")
-		public void acessar_a_pagina_de_cadastro_de_categoria() {
-			// TODO
-		}
+import helpers.ChromeHelper;
+import helpers.ScreenShotHelper;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 
-		@E("Informar o nome da categoria")
-		public void informar_o_nome_da_categoria() {
-			// TODO
-		}
+public class CadastrarCategoriasStepDefinitions {
 
-		@Quando("Solicitar a realização do cadastro da categoria")
-		public void solicitar_a_realizacao_do_cadastro_da_categoria() {
-			// TODO
-		}
+	WebDriver driver;
 
-		@Então("Sistema informa que a categoria foi cadastrada com sucesso")
-		public void sistema_informa_que_a_categoria_foi_cadastrada_com_sucesso() {
-			// TODO
-		}
+	@Dado("Acessar a página de cadastro de categoria")
+	public void acessar_a_pagina_de_cadastro_de_categoria() {
+		driver = ChromeHelper.create("http://appcontascoti-001-site1.dtempurl.com/Categorias/Cadastro");
 	}
 
+	@E("Informar o nome da categoria")
+	public void informar_o_nome_da_categoria() {
+		driver.findElement(By.xpath("//*[@id=\"Nome\"]")).sendKeys("Categoria Teste");
+	}
+
+	@Quando("Solicitar a realização do cadastro da categoria")
+	public void solicitar_a_realizacao_do_cadastro_da_categoria() {
+		driver.findElement(By.xpath("/html/body/div/div/div/form/div[2]/input")).click();
+	}
+
+	@Então("Sistema informa que a categoria foi cadastrada com sucesso")
+	public void sistema_informa_que_a_categoria_foi_cadastrada_com_sucesso() {
+		
+		ScreenShotHelper.create(driver, "Cadastrar categoria.png");
+		
+		String mensagem = driver.findElement(By.xpath("/html/body/div[1]")).getText();
+		assertTrue(mensagem.contains("cadastrada com sucesso"));
+	}
+}
--- src/test/java/cotiinformatica/CadastrarContasStepDefinitions.java
+++ src/test/java/cotiinformatica/CadastrarContasStepDefinitions.java
@@ -1,54 +1,76 @@
 package cotiinformatica;
 
-	import io.cucumber.java.pt.E;
-	import io.cucumber.java.pt.Então;
-	import io.cucumber.java.pt.Quando;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-	public class CadastrarContasStepDefinitions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
-		@Quando("Acessar a página de cadastro de contas")
-		public void acessar_a_pagina_de_cadastro_de_contas() {
-			// TODO
+import helpers.ChromeHelper;
+import helpers.ScreenShotHelper;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class CadastrarContasStepDefinitions {
+
+	WebDriver driver;
+	
+	@Quando("Acessar a página de cadastro de contas")
+	public void acessar_a_pagina_de_cadastro_de_contas() {
+		driver = ChromeHelper.create("http://appcontascoti-001-site1.dtempurl.com/Contas/Cadastro");
+	}
+
+	@E("Informar o nome da conta {string}")
+	public void informar_o_nome_da_conta(String nome) {
+		driver.findElement(By.xpath("//*[@id=\"Nome\"]")).sendKeys(nome);
+	}
+
+	@E("Informar o valor da conta {string}")
+	public void informar_o_valor_da_conta(String valor) {
+		driver.findElement(By.xpath("//*[@id=\"Valor\"]")).sendKeys(valor);
+	}
+
+	@E("Informar a data da conta {string}")
+	public void informar_a_data_da_conta(String data) {
+		driver.findElement(By.xpath("//*[@id=\"Data\"]")).sendKeys(data);
+	}
+
+	@E("Selecionar o tipo da conta {string}")
+	public void selecionar_o_tipo_da_conta(String tipo) {
+		if(tipo.equals("Conta a Receber")) {
+			driver.findElement(By.xpath("//*[@id=\"Tipo\"]")).click();
 		}
-
-		@E("Informar o nome da conta {string}")
-		public void informar_o_nome_da_conta(String nome) {
-			// TODO
-		}
-
-		@E("Informar o valor da conta {string}")
-		public void informar_o_valor_da_conta(String valor) {
-			// TODO
-		}
-
-		@E("Informar a data da conta {string}")
-		public void informar_a_data_da_conta(String data) {
-			// TODO
-		}
-
-		@E("Selecionar o tipo da conta {string}")
-		public void selecionar_o_tipo_da_conta(String tipo) {
-			// TODO
-		}
-
-		@E("Selecionar a categoria da conta {string}")
-		public void selecionar_a_categoria_da_conta(String categoria) {
-			// TODO
-		}
-
-		@E("Preencher as observações da conta {string}")
-		public void preencher_as_observacoes_da_conta(String observacoes) {
-			// TODO
-		}
-
-		@Quando("Solicitar a realização do cadastro da conta")
-		public void solicitar_a_realizacao_do_cadastro_da_conta() {
-			// TODO
-		}
-
-		@Então("Sistema informa que a conta foi cadastrada com sucesso")
-		public void sistema_informa_que_a_conta_foi_cadastrada_com_sucesso() {
-			// TODO
+		else if(tipo.equals("Conta a Pagar")) {
+			driver.findElement(By.xpath("//*[@id=\"Tipo\"]")).click();
 		}
 	}
+
+	@E("Selecionar a categoria da conta {string}")
+	public void selecionar_a_categoria_da_conta(String categoria) {
+		Select select = new Select(driver.findElement(By.xpath("//*[@id=\"IdCategoria\"]")));
+		select.selectByVisibleText(categoria);
+	}
+
+	@E("Preencher as observações da conta {string}")
+	public void preencher_as_observacoes_da_conta(String observacoes) {
+		driver.findElement(By.xpath("//*[@id=\"Observacoes\"]")).sendKeys(observacoes);
+	}
+
+	@Quando("Solicitar a realização do cadastro da conta")
+	public void solicitar_a_realizacao_do_cadastro_da_conta() {
+		driver.findElement(By.xpath("/html/body/div/div/div/form/div[4]/div/input")).click();
+	}
+
+	@Então("Sistema informa que a conta foi cadastrada com sucesso")
+	public void sistema_informa_que_a_conta_foi_cadastrada_com_sucesso() {
+		
+		ScreenShotHelper.create(driver, "Cadastrar conta.png");
+		
+		String mensagem = driver.findElement(By.xpath("/html/body/div[1]")).getText();
+		assertTrue(mensagem.contains("cadastrada com sucesso"));
+	}
+}
+
+
 
